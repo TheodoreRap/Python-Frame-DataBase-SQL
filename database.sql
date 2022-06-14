ad00273b71f248851175e95e561020ee522514cf
Theodore R <103283373+TheodoreRap@users.noreply.github.com>
2022-06-14 22:47:28 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,21 +1,6 @@
 -- phpMyAdmin SQL Dump
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
---
--- Εξυπηρετητής: 127.0.0.1
--- Χρόνος δημιουργίας: 14 Ιουν 2022 στις 21:45:55
--- Έκδοση διακομιστή: 10.4.22-MariaDB
--- Έκδοση PHP: 8.0.13
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Βάση δεδομένων: `database`
@@ -35,26 +20,10 @@ CREATE TABLE `person` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Άδειασμα δεδομένων του πίνακα `person`
---
-
-INSERT INTO `person` (`id`, `name`, `x`, `y`) VALUES
-(1, 'a', 12, 32),
-(2, 'b', 32, 43);
-
---
--- Ευρετήρια για άχρηστους πίνακες
---
-
---
 -- Ευρετήρια για πίνακα `person`
 --
 ALTER TABLE `person`
   ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT για άχρηστους πίνακες
---
 
 --
 -- AUTO_INCREMENT για πίνακα `person`
@@ -62,7 +31,3 @@ ALTER TABLE `person`
 ALTER TABLE `person`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
